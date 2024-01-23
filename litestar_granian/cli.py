37e7de36b92c7ce869d227ce54db4cb96b83d3b3
Cody Fincher <cody.fincher@gmail.com>
2024-01-22 23:46:22 -0600

    feat: updated to 1.0 api

--- litestar_granian/cli.py
+++ litestar_granian/cli.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 import click
 from click import Context, command, option
 from granian.constants import HTTPModes, ThreadModes
+from granian.http import HTTP1Settings, HTTP2Settings
 
 if TYPE_CHECKING:
     from litestar import Litestar
@@ -14,7 +15,6 @@ if TYPE_CHECKING:
 
 
 @command(name="run")
-@option("-r", "--reload", help="Reload server on changes", default=False, is_flag=True)
 @option("-p", "--port", help="Serve under this port", type=int, default=8000, show_default=True)
 @option(
     "-W",
@@ -29,6 +29,13 @@ if TYPE_CHECKING:
 @option(
     "--threads",
     help="The number of threads.",
+    type=click.IntRange(min=1),
+    show_default=True,
+    default=1,
+)
+@option(
+    "--blocking-threads",
+    help="The number of blocking threads.",
     type=click.IntRange(min=1),
     show_default=True,
     default=1,
@@ -63,15 +70,118 @@ if TYPE_CHECKING:
     help="If certificate and key are not found at specified locations, create a self-signed certificate and a key",
     is_flag=True,
 )
+@option(
+    "--http1-buffer-size",
+    help="Set the maximum buffer size for HTTP/1 connections",
+    type=click.IntRange(min=8192),
+    show_default=True,
+    default=HTTP1Settings.max_buffer_size,
+)
+@option(
+    "--http1-keep-alive/--no-http1-keep-alive",
+    help="Enables or disables HTTP/1 keep-alive",
+    default=HTTP1Settings.keep_alive,
+    show_default=True,
+    is_flag=True,
+)
+@option(
+    "--http1-pipeline-flush/--no-http1-pipeline-flush",
+    help="Aggregates HTTP/1 flushes to better support pipelined responses (experimental)",
+    default=False,
+    is_flag=True,
+)
+@option(
+    "--http2-adaptive-window/--no-http2-adaptive-window",
+    help="Sets whether to use an adaptive flow control for HTTP2",
+    default=HTTP2Settings.adaptive_window,
+    is_flag=True,
+)
+@option(
+    "--http2-initial-connection-window-size",
+    help="Sets the max connection-level flow control for HTTP2",
+    type=int,
+    show_default=False,
+    default=HTTP2Settings.initial_connection_window_size,
+)
+@option(
+    "--http2-initial-stream-window-size",
+    help="Sets the `SETTINGS_INITIAL_WINDOW_SIZE` option for HTTP2 stream-level flow control",
+    type=int,
+    show_default=False,
+    default=HTTP2Settings.initial_stream_window_size,
+)
+@option(
+    "--http2-keep-alive-interval",
+    help="Sets an interval for HTTP2 Ping frames should be sent to keep a connection alive",
+    type=int,
+    show_default=False,
+    default=HTTP2Settings.keep_alive_interval,
+)
+@option(
+    "--http2-keep-alive-timeout",
+    help="Sets a timeout for receiving an acknowledgement of the HTTP2 keep-alive ping",
+    type=int,
+    show_default=False,
+    default=HTTP2Settings.keep_alive_timeout,
+)
+@option(
+    "--http2-max-concurrent-streams",
+    help="Sets the SETTINGS_MAX_CONCURRENT_STREAMS option for HTTP2 connections",
+    type=int,
+    show_default=False,
+    default=HTTP2Settings.max_concurrent_streams,
+)
+@option(
+    "--http2-max-frame-size",
+    help="Sets the maximum frame size to use for HTTP2",
+    type=int,
+    show_default=False,
+    default=HTTP2Settings.max_frame_size,
+)
+@option(
+    "--http2-max-headers-size",
+    help="Sets the max size of received header frames",
+    type=int,
+    show_default=False,
+    default=HTTP2Settings.max_headers_size,
+)
+@option(
+    "--http2-max-send-buffer-size",
+    help="Set the maximum write buffer size for each HTTP/2 stream",
+    type=int,
+    show_default=False,
+    default=HTTP2Settings.max_send_buffer_size,
+)
 @option("--url-path-prefix", help="URL path prefix the app is mounted on", default=None, show_default=False)
 @option("-d", "--debug", help="Run app in debug mode", is_flag=True)
 @option("-P", "--pdb", "--use-pdb", help="Drop into PDB on an exception", is_flag=True)
+@option(
+    "--respawn-failed-workers/--no-respawn-failed-workers",
+    help="Enable workers respawn on unexpected exit",
+    default=False,
+    is_flag=True,
+)
+@option("-r", "--reload", help="Reload server on changes", default=False, is_flag=True)
 def run_command(
     app: Litestar,
     reload: bool,
     port: int,
     wc: int,
     threads: int,
+    blocking_threads: int,
+    respawn_failed_workers: bool,
+    http1_keep_alive: bool,
+    http1_buffer_size: int,
+    http1_pipeline_flush: bool,
+    http2_adaptive_window: bool,
+    http2_initial_connection_window_size: int,
+    http2_initial_stream_window_size: int,
+    http2_keep_alive_interval: int | None,
+    http2_keep_alive_timeout: int,
+    http2_max_concurrent_streams: int,
+    http2_max_frame_size: int,
+    http2_max_headers_size: int,
+    http2_max_send_buffer_size: int,
     http: HTTPModes,
     no_opt: bool,
     backlog: int,
@@ -136,6 +246,20 @@ def run_command(
         http=http,
         no_opt=no_opt,
         backlog=backlog,
+        blocking_threads=blocking_threads,
+        respawn_failed_workers=respawn_failed_workers,
+        http1_buffer_size=http1_buffer_size,
+        http1_keep_alive=http1_keep_alive,
+        http1_pipeline_flush=http1_pipeline_flush,
+        http2_adaptive_window=http2_adaptive_window,
+        http2_initial_connection_window_size=http2_initial_connection_window_size,
+        http2_initial_stream_window_size=http2_initial_stream_window_size,
+        http2_keep_alive_interval=http2_keep_alive_interval,
+        http2_keep_alive_timeout=http2_keep_alive_timeout,
+        http2_max_concurrent_streams=http2_max_concurrent_streams,
+        http2_max_frame_size=http2_max_frame_size,
+        http2_max_headers_size=http2_max_headers_size,
+        http2_max_send_buffer_size=http2_max_send_buffer_size,
         threading_mode=threading_mode,
         ssl_keyfile=ssl_keyfile,
         ssl_certificate=ssl_certificate,
@@ -153,6 +277,20 @@ def _run(
     http: HTTPModes,
     no_opt: bool,
     backlog: int,
+    blocking_threads: int,
+    respawn_failed_workers: bool,
+    http1_buffer_size: int,
+    http1_keep_alive: bool,
+    http1_pipeline_flush: bool,
+    http2_adaptive_window: bool,
+    http2_initial_connection_window_size: int,
+    http2_initial_stream_window_size: int,
+    http2_keep_alive_interval: int | None,
+    http2_keep_alive_timeout: int,
+    http2_max_concurrent_streams: int,
+    http2_max_frame_size: int,
+    http2_max_headers_size: int,
+    http2_max_send_buffer_size: int,
     threading_mode: ThreadModes,
     ssl_keyfile: Path | None,
     ssl_certificate: Path | None,
@@ -191,7 +329,7 @@ def _run(
             interface=Interfaces.ASGI,
             workers=wc,
             threads=threads,
-            pthreads=threads,
+            pthreads=blocking_threads,
             threading_mode=threading_mode,
             loop=Loops.uvloop,
             loop_opt=not no_opt,
@@ -201,6 +339,23 @@ def _run(
             http=http,
             websockets=True,
             backlog=backlog,
+            respawn_failed_workers=respawn_failed_workers,
+            http1_settings=HTTP1Settings(
+                keep_alive=http1_keep_alive,
+                max_buffer_size=http1_buffer_size,
+                pipeline_flush=http1_pipeline_flush,
+            ),
+            http2_settings=HTTP2Settings(
+                adaptive_window=http2_adaptive_window,
+                initial_connection_window_size=http2_initial_connection_window_size,
+                initial_stream_window_size=http2_initial_stream_window_size,
+                keep_alive_interval=http2_keep_alive_interval,
+                keep_alive_timeout=http2_keep_alive_timeout,
+                max_concurrent_streams=http2_max_concurrent_streams,
+                max_frame_size=http2_max_frame_size,
+                max_headers_size=http2_max_headers_size,
+                max_send_buffer_size=http2_max_send_buffer_size,
+            ),
             reload=reload,
             ssl_cert=ssl_certificate,
             ssl_key=ssl_keyfile,
